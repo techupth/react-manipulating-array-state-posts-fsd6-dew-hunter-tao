fab485fc9af5626a42cc606ad9024a98f4f6d53b
Hunter <hunvicter@gmail.com>
2024-06-06 15:36:46 +0700

    Like-Dislike

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
+import Data from "./info";
+
 function Posts() {
+  let like = Data.map((item) => item.likes);
+  const [counter, setCounter] = useState(like);
+
+  const handleLike = (index) => {
+    let resultLike = [...counter];
+    resultLike[index] +=1;
+    setCounter(resultLike);
+  };
+  const handleDislike = (index) => {
+    let resultDislike = [...counter];
+    if (resultDislike[index] <= 0) {
+      resultDislike[index] = 0;
+    } else {
+      resultDislike[index] -= 1;
+    }
+    setCounter(resultDislike);
+  };
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {Data.map((item, index) => {
+          return (
+            <div class="post-item" key={index}>
+              <div class="post-header">
+                <h2>
+                  {item.title} #{item.id}
+                </h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{counter[index]}</span>
+                </div>
+              </div>
+              <p class="post-content">{item.content}</p>
+              <div class="post-actions">
+                <button
+                  class="like-button"
+                  onClick={() => {
+                    handleLike(index);
+                  }}
+                >
+                  Like
+                </button>
+                <button
+                  class="dislike-button"
+                  onClick={() => {
+                    handleDislike(index);
+                  }}
+                >
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
