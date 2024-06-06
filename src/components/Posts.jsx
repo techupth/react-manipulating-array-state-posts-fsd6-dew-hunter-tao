3aaaa620ad1110673d01792506f472b1ebb0eb52
Prechak <benedict.precha@gmail.com>
2024-06-06 12:19:01 +0700

    feat: like count

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,26 +1,44 @@
+import { useState } from "react";
+
 function Posts() {
+  const [like, setLike] = useState(0);
+
+  const handleLike = () => {
+    setLike(like + 1);
+  };
+
+  const handleDislike = () => {
+    if (like > 0) {
+      setLike(like - 1);
+    }
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="post-list">
+        <div className="post-item">
+          <div className="post-header">
             <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+            <div className="post-social-media-stats">
+              <span className="stats-topic">Likes: </span>
+              <span className="post-likes">{like}</span>
             </div>
           </div>
-          <p class="post-content">
+          <p className="post-content">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
             vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
             tincidunt, leo non molestie consectetur, elit libero faucibus
             tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
             condimentum mauris euismod pellentesque eu eu justo...
           </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+          <div className="post-actions">
+            <button className="like-button" onClick={handleLike}>
+              Like
+            </button>
+            <button className="dislike-button" onClick={handleDislike}>
+              Dislike
+            </button>
           </div>
         </div>
       </div>
